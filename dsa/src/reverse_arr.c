6ec4695a4821116c53647fa5c0196a3f222fc7e1
lkessels <l@lmpkessels.com>
2026-04-20 13:30:18 +0200

    docs(reverse): describe the functionallity

--- dsa/src/reverse_arr.c
+++ dsa/src/reverse_arr.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+// Starts at the end of the array and moves back down to copy each
+// piece of data one-by-one into temp
+// 
+// Then it copies the data from temp, and stores it one-by-one back 
+// into arr[]
 int reverse_arr(int arr[], int n) {
     int temp[n];
 
@@ -20,6 +25,9 @@ int swap(int *a, int *b) {
     printf("a: %d, b: %d\n", *a, *b);
 }
 
+// Has both a left side and right side of the array (0, 10; 1, 9; etc...) 
+// those are getting swapped (10, 0; 9, 1;) this gives us instead of 10 
+// steps (in this use case) 5 steps
 int reverse_arr_w_swap(int arr[], int n) {
     int left = 0, right = n - 1;
 
@@ -34,6 +42,7 @@ int reverse_arr_w_swap(int arr[], int n) {
 
 int main() {
     int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    // Get the exact size of index positions (40 / 4 = 10)
     int n = sizeof(arr) / sizeof(arr[10]);
 
     reverse(arr, n);
